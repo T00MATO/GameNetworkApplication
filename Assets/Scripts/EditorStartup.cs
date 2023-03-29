bffa0c02f1bdf23023daf3fdf083e477ce99ae3c
T00MATO <toomato030517@gmail.com>
2023-03-29 11:17:26 +0900

    에디터 스타트업 변경

---
+++ Assets/Scripts/EditorStartup.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public static class EditorStartup
+{
+    public static readonly string START_SCENE_PATH = "Assets/Scenes/LoginScene.unity";
+
+    static EditorStartup()
+    {
+        var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(START_SCENE_PATH);
+        EditorSceneManager.playModeStartScene = sceneAsset;
+    }
+}
+#endif
